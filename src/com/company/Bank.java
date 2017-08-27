43b3f21327e72937d0b4cfcfb9336418b8a64c7e
filipe <filiperamalho@bitbucket.com>
2017-08-27 00:52:05 -0300

    Criação do repositório

---
+++ src/com/company/Bank.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Bank {
+    private int[] Accounts;
+    private long Transactions;
+
+    public Bank(int NumberOfAccounts,int InitialBalance){
+        this.Accounts = new int[NumberOfAccounts];
+        for (int i=0;i<NumberOfAccounts;i++){
+            this.Accounts[i]=InitialBalance;
+        }
+        this.Transactions=0;
+    }
+
+    public int[] getAccounts() {
+        return Accounts;
+    }
+
+    public void setAccounts(int[] accounts) {
+        Accounts = accounts;
+    }
+
+    public long getTransactions() {
+        return Transactions;
+    }
+
+    public void setTransactions(long transactions) {
+        Transactions = transactions;
+    }
+
+    public void transfer(int from, int to, int amount){
+        if(this.Accounts[from] >= amount){
+            this.Accounts[from] -= amount;
+            this.Accounts[to] += amount;
+        }else{
+            System.err.println("Saldo insuficiente para realizar transação\n");
+        }
+
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static int NUMBEROFACCOUNTS;
+    public static int INITIALBALANCE;
+    public static void main(String[] args) {
+
+    }
+}
